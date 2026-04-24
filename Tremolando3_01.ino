43d290fe29fecf423e60e41cff6464c418d529a7
sindregjone <sindregjone@outlook.com>
2026-04-24 12:07:13 +0200

    Fix OLED contrast: use ssd1306_command directly

--- Tremolando3_01.ino
+++ Tremolando3_01.ino
@@ -212,7 +212,8 @@ void setup() {
   sgtl5000_1.autoVolumeDisable();*/
  // Serial.begin(115200);
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 128x64)
-  display.setContrast(255);
+  display.ssd1306_command(SSD1306_SETCONTRAST);
+  display.ssd1306_command(255);
   display.clearDisplay();
   pixels.setPixelColor(0,yellow);
   pixels.show();
